ad06da96e7313a9069553946553559558e249bd8
PhyoeSandy <phyoesandypink123@gmail.com>
2020-07-13 10:53:45 +0630

    feat: add ui test for play game button

---
+++ BurgerJoint/app/src/androidTest/java/com/zg/burgerjoint/ui_tests/burgerlist/PlayGameTest.kt
@@ -0,0 +1,39 @@
+package com.zg.burgerjoint.ui_tests.burgerlist
+
+import android.content.Intent
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.rule.ActivityTestRule
+import com.zg.burgerjoint.R
+import com.zg.burgerjoint.activities.MainActivity
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Created by Phyoe Sandy Soe Tun
+ * on 7/13/2020.
+ */
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class PlayGameTest {
+    private val activityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Before
+    fun setUp() {
+        activityTestRule.launchActivity(Intent())
+    }
+
+    @Test
+    fun tapOnPlayGame_navigateToGameScreen() {
+        onView(withId(R.id.btnPlayGame))
+            .perform(click())
+        onView(withId(R.id.ivGameBurger))
+            .check(matches(isDisplayed()))
+    }
+
+}
